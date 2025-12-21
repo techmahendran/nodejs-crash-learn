103f0e1991cf9c20fcd20c325ae6b34d8418680f
Mahendran M <mahendran.m@impacteers.com>
2025-12-21 23:02:58 +0530

    node js changes

--- node_practice/server.js
+++ node_practice/server.js
@@ -22,5 +22,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port http://localhost:${PORT}`);
 });
